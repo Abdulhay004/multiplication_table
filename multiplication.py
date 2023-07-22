b14a11e53d6fe4d4e2cc56766c378f75d3cfb89e
Abdulhay004 <119282092+Abdulhay004@users.noreply.github.com>
2023-07-22 23:46:07 +0500

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -1,1 +1,10 @@
+def jadval(num):
+    print(' ', *list(range(1, num+1)), sep='  ')
+    for i in range(1, num+1):
+        print(i, end=' ')
+        for j in range(1, num+1):
+            print(f'{i*j: >2}', end=' ')
+        print()
 
+num = int(input())
+jadval(num)
